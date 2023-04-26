bffd0a79b4577bd3b3206568e2631c394aca328b
capyana <97465942+capyana@users.noreply.github.com>
2023-04-26 12:57:25 -0700

    lab 5- defined javascript variables

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - pseudocoding rock paper scissors
+//Rock-Paper-Scissors
+// Author: Tyana Tran( ttran192@ucsc.edu) and Oliver Mason 
+// Date: 4/24/23
 
 // Constants
 
 // Functions
+//Rock-Paper-Scissors
+//Print "Select Rock Paper or Scissors (R, P, S)"
+  //Save input to variable
+    //Validate input to see if (Rock, rock, r, R, Paper, paper, P, p, Scissors, scissors, S, s)
+    //if not restart
+//Computer makes a random choice of Rock Paper Scissors
+//Compare user and computer results
+//Print results
+//Prompt for a new game
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables 
+// Author: Tyana Tran
+// Date:4/26/23
 
 // Constants
 
 // Functions
+
+// Define Variables 
+make = "Hyundai";
+model= "Elantra";
+color= "Black";
+year= "2018";
+// Calculate 
+age = 2019 - year;
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
